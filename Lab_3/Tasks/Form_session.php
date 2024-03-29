4a649c9369a338112bd99054abcac66d6ceb6fc2
Lizapeach <118962019+Lizapeach@users.noreply.github.com>
2024-03-29 23:30:22 +0200

    Task_2_c

--- Lab_3/Tasks/Form_session.php
+++ Lab_3/Tasks/Form_session.php
@@ -5,7 +5,9 @@ if(isset($_POST['submit'])){
     $data = [
         'фамилия' => $_POST['фамилия'],
         'имя' => $_POST['имя'],
-        'возраст' => $_POST['возраст']
+        'возраст' => $_POST['возраст'],
+        'пол' => $_POST['пол'],
+        'город' => $_POST['город']
     ];
 
     $_SESSION['userData'] = $data;
@@ -25,6 +27,8 @@ if(isset($_POST['submit'])){
     Фамилия: <input type="text" name="фамилия"><br><br>
     Имя: <input type="text" name="имя"><br><br>
     Возраст: <input type="text" name="возраст"><br><br>
+    Пол: <input type="text" name="пол"><br><br>
+    Город: <input type="text" name="город"><br><br>
     <input type="submit" name="submit" value="Отправить">
 </form>
 </body>
--- Lab_3/Tasks/Output.php
+++ Lab_3/Tasks/Output.php
@@ -1,13 +1,28 @@
 <?php
+//For Task 2_c
 session_start();
 
 if(isset($_SESSION['userData'])){
-    $userData = $_SESSION['userData'];
     echo "<h2>Данные пользователя:</h2>";
-    echo "Фамилия: " . $userData['фамилия'] . "<br>";
-    echo "Имя: " . $userData['имя'] . "<br>";
-    echo "Возраст: " . $userData['возраст'] . "<br>";
+    echo "<ul>";
+    foreach($_SESSION['userData'] as $key => $value){
+        echo "<li><strong>$key:</strong> $value</li>";
+    }
+    echo "</ul>";
 } else {
     echo "Данные не найдены.";
 }
+
+//For Task 2_b
+//session_start();
+//
+//if(isset($_SESSION['userData'])){
+//    $userData = $_SESSION['userData'];
+//    echo "<h2>Данные пользователя:</h2>";
+//    echo "Фамилия: " . $userData['фамилия'] . "<br>";
+//    echo "Имя: " . $userData['имя'] . "<br>";
+//    echo "Возраст: " . $userData['возраст'] . "<br>";
+//} else {
+//    echo "Данные не найдены.";
+//}
 
