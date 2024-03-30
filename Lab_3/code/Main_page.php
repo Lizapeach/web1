77157c65a4f74562d3086b50e86de110249f8d49
Lizapeach <118962019+Lizapeach@users.noreply.github.com>
2024-03-30 19:46:39 +0200

    adjustments

---
+++ Lab_3/code/Main_page.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Главная страница</title>
+</head>
+<body>
+<h1>Добро пожаловать на главную страницу!</h1>
+
+    <button onclick="window.location.href='Regular_expressions.php'">Регулярные выражения</button><br><br>
+    <button onclick="window.location.href='Сounting_words_characters.php'">Подсчет слов и символов</button><br><br>
+    <button onclick="window.location.href='Form_session.php'">Заполнение формы</button><br><br>
+    <button onclick="window.location.href='Files.php'">Документ</button>
+
+
+</body>
+</html>
