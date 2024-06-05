0e97963e38fd7188d2d4c66aaf68eab21cf90d84
Lizapeach <118962019+Lizapeach@users.noreply.github.com>
2024-06-05 21:26:50 +0200

    add a form for creating ads and a table

--- Lab_5/code/code.php
+++ Lab_5/code/code.php
@@ -24,3 +24,60 @@ $stmt->close();
 
 $mysqli->close();
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Лаба_5</title>
+</head>
+<body>
+<div id="form">
+    <form action="code.php" method="post">
+        <label for="email">Email</label>
+        <input type="email" name="email" required>
+
+        <label for="categories">Category</label>
+        <select name="categories" required>
+
+            <option value="Products">Products</option>
+            <option value="Toys">Toys</option>
+            <option value="Utensils">Utensils</option>
+        </select><br>
+
+        <label for="title">Title:</label><br>
+        <input type="text" id="title" name="title" required><br>
+
+        <label for="text">Description:</label><br>
+        <textarea rows="4" name="text" required></textarea><br>
+
+        <input type="submit" value="Save"><br>
+    </form>
+</div>
+<div id="table">
+    <table>
+        <thead>
+        <th>Email</th>
+        <th>Category</th>
+        <th>Title</th>
+        <th>Description</th>
+        </thead>
+        <tbody>
+        <?php
+        $rows = array_map(function($advertisement) {
+            return '<tr>
+        <td>' . $advertisement['email'] . '</td>
+        <td>' . $advertisement['categories'] . '</td>
+        <td>' . $advertisement['title'] . '</td>
+        <td>' . $advertisement['text'] . '</td>
+    </tr>';
+        }, $ads);
+        echo implode('', $rows);
+        ?>
+        </tbody>
+    </table>
+</div>
+</body>
+</html>
